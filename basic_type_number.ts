a5f559bffd55268656cbdaeebde4cd17e947df6d
Equan Pr <equan.p@gmail.com>
2020-06-05 12:05:47 +0700

    console out numbers

--- basic_type_number.ts
+++ basic_type_number.ts
@@ -9,3 +9,17 @@ let hex: number = 0xf00d;
 let binary: number = 0b1110;
 let octal: number = 0o764;
 
+let isHexNumber:boolean = true;
+let isBinaryNumber: boolean= true;
+
+console.log(decimal);
+
+if(isHexNumber){
+  console.log(`0x${hex.toString(16)}`);
+}
+
+if(isBinaryNumber) {
+  console.log(`0b${binary.toString(2)}`);
+}
+
+console.log(`0o${octal.toString(8)}`);
